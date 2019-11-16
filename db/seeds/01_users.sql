b8b3d7070bee1c9e90ce0ecc2b6f288aedebc8c9
HermSidhu <h-sidhu@hotmail.com>
2019-11-16 22:16:49 +0000

    test

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, phone, email, password, profile_pic_URL)
+VALUES ('Hungry Guy', '905-123-1234', 'hungryguy@gmail.com', 'hunger', 'database_photos/users/002-meat.eps');
---
+++ db/seeds/02_restaurants.sql
@@ -0,0 +1,2 @@
+INSERT INTO restaurants (title, cuisine_type, hours_of_operations, cover_photo_operations, cover_photo_url, slide_1_url, slide_2_url, slide_3_url, street, city, province, post_code, country)
+VALUES ('Porchetta & Co.', );
