a2914bf40a081aae6bd84423b4ff27fe2996bbe7
HermSidhu <h-sidhu@hotmail.com>
2019-11-18 16:37:52 +0000

    populating databases

--- db/schema/05_orders.sql
+++ db/schema/05_orders.sql
@@ -4,5 +4,6 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
+  total AS SUM(price) FROM menu_items,
   status TIMESTAMP
 );
--- db/seeds/03_menu_items.sql
+++ db/seeds/03_menu_items.sql
@@ -6,8 +6,39 @@ INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remai
 VALUES (1, 'O.G Fried Chicken Sandwich', 'Franks Red Hot Sauce, lettuce, pickles & fresh herb ranch dressing.', 10.95, 10);
 
 INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (1, 'Porchetta Plate', 'Hand carved porchetta with crackling.', 12.95, 20);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (1, 'Fried Chicken Parmesan Sandwich', 'Tomato sauce, mozzarella & parmesan cheese on a toasted garlic bread bun.', 10.95, 30);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
 VALUES (1, 'French Fries', 'Freshly cut fried potatoes.', 3.95, 50);
 
 INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
-VALUES (1, 'Porchetta Plate', 'Hand carved porchetta with crackling.', 12.95, 20);
+VALUES (1, 'Baked Beans', 'Fresh canned beans baked on the stove top, yumm...', 3.95, 40);
 
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (1, 'Perrier', 'Sparkling Water', 2.25, 50);
+
+/* BIG SMOKE BURGER */
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'The Classic Burger', 'Canadian AAA beef, char-grilled & hand crafted. Made fresh daily. Served with lettuce, tomatoes, pickles, onions, ketchup, mustard & mayo.', 7.25, 50);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'Smokey Bacon Burger', 'Smoked Canadian cheddar, bacon, lettuce, tomatoes, pickle, onions, ketchup, mustard & mayo.', 10.95, 50);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'Blue Burger', 'Avocado, gorgonzola, house-made rosemary-garlic mayo, lettuce & tomatoes.', 9.55, 50);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'Onion Rings', 'Crispy, breaded & lightly fried.', 5.15, 50);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'Garden Salad', 'Fresh garden salad served with lettuce, carrots, tomatoes & house dressing.', 4.10, 50);
+
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (2, 'Old Fashioned Milkshake', 'Strawberry, vanilla or chocolate.', 5.35, 50);
+
+/* Coco Rice Thai Cuisine */
+INSERT INTO menu_items (restaurant_id, title, description, price, quantity_remaining)
+VALUES (1, 'Perrier', 'Sparkling Water', 2.25, 50);
