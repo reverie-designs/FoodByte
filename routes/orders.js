730e690c45281bed8b9af3dcf3a1a690389dcaab
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-18 00:21:37 +0000

    removed error lines in orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -9,22 +9,7 @@ module.exports = (db) => {
     // req.session.user_id = req.params.id;
     res.redirect("/restaurants");
   });
-  const rest = {
-    owner_id: 2,
-    title: "the greatest restaurant",
-    cuisine_type: "human edible",
-    hours_of_operation: "forever",
-    cover_photo_url: 'https://images.unsplash.com/photo-1573920111312-04f1b25c6b85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    slide_1_url: 'https://images.unsplash.com/photo-1573853403795-8755533bcd87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    slide_2_url: 'https://images.unsplash.com/photo-1573853403795-8755533bcd87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    slide_3_url: 'https://images.unsplash.com/photo-1573853403795-8755533bcd87?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    street: '23 baker street',
-    city: 'toronto',
-    province: 'Ontario',
-    post_code: 'LW4 5t6',
-    country: 'canada',
-    active: 'true'
-  };
+
   // router.post("/orders", (req, res) => {
   //   db.query(`SELECT * FROM users;`)
   //     .then(data => {
