164e329dadedac51c1fb6f5d9bc6f99f7d29ce59
HermSidhu <h-sidhu@hotmail.com>
2019-11-19 22:08:08 +0000

    Dont pull request this, sms works

--- helpers/dataHelpers.js
+++ helpers/dataHelpers.js
@@ -35,11 +35,22 @@ module.exports = (db) => ({
    */
   getAllRestaurantMenuItems: function(restaurant_id) {
     return db.query(`
-    SELECT menu_items.title AS itemName, *
+    SELECT menu_items.id AS menuid, menu_items.title AS itemName, *
     FROM menu_items
     JOIN restaurants ON restaurants.id = restaurant_id
     WHERE menu_items.restaurant_id = $1;
     `, [restaurant_id])
+      .then(res => {
+        console.log(res.rows);
+        return res.rows;
+      });
+  },
+
+  getOrder: function(order_id) {
+    return db.query(`
+    SELECT * FROM orders
+    WHERE id = $1;
+    `, [order_id])
       .then(res => {
         console.log(res.rows);
         return res.rows;
--- send-sms.js
+++ send-sms.js
@@ -1,9 +1,16 @@
+const dh = require('./helpers/dataHelpers.js');
 const accountSid = 'AC2d799e3b41b15182aa89ad2eca682d28'
 const authToken = '28705347311b7d8df12c161f50790ca7'
 
 const client = require('twilio')(accountSid, authToken);
 
-client.messages.create({
+module.exports = (db) =>({
+  sendSMS: function() {
+  dh(db).getOrder(1)
+  .then(orders => {
+    console.log(orders);
+  });
+  client.messages.create({
   to:   '+16477004837',
   from: '+16479300219',
   body: 'Your order will be ready in 15 mins'
@@ -13,7 +20,9 @@ client.messages.create({
 client.messages.create({
   to:   '+14372219197',
   from: '+16479300219',
-  body: 'NEW ORDER: '
+  body: `NEW ORDER ${console.log(.then(orders => {
+    console.log(orders);)}`
 })
 .then((message) => console.log(message.sid));
-
+  }
+});
