abed2255c642fdb5ea377d9482aa594e18cc10f1
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-20 19:31:32 +0000

    working on twillio

---
+++ send-sms.js
@@ -0,0 +1,28 @@
+const dh = require('./helpers/dataHelpers.js');
+const accountSid = 'AC2d799e3b41b15182aa89ad2eca682d28'
+const authToken = '28705347311b7d8df12c161f50790ca7'
+
+const client = require('twilio')(accountSid, authToken);
+
+module.exports = (db) =>({
+  sendSMS: function() {
+  dh(db).getOrder(1)
+  .then(orders => {
+    console.log(orders);
+  });
+  client.messages.create({
+  to:   '+16477004837',
+  from: '+16479300219',
+  body: 'Your order will be ready in 15 mins'
+})
+.then((message) => console.log(message.sid));
+
+client.messages.create({
+  to:   '+14372219197',
+  from: '+16479300219',
+  body: `NEW ORDER ${console.log(.then(orders => {
+    console.log(orders);)}`
+})
+.then((message) => console.log(message.sid));
+  }
+});
