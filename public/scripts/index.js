fdde42945456acff40087b956c01b9a065c97c1d
Dasha Frumovitch <dfrumovitch@gmail.com>
2019-11-21 01:22:45 +0000

    added function to sum item total

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,26 +1,66 @@
 const addOrderItem = (title, price, id) =>{
-  return `<div class="dp-flex justify-content-between text-light">
+  return `<div class="dp-flex justify-content-between text-light ord-item">
   <label class="pr-2">${title}</label>
-  <input type="number" name="${id}" min="0" max="100" step="1" size="2" value="1"><br> <span>${price}</span>
+  <input data-input-id="${id}" type="number" name="${id}" min="0" max="100" step="1" size="2" class="float-right quantity"><br> <p class="text-right clear-fix w-100 item-price">${price}</p>
   </div>`;
 };
 let total = 0;
+let value = 1;
+
+const getTotal = (price) => {
+  total = total + (price);
+  let itemTotal = total.toFixed(2);
+  let tax = (itemTotal * 0.13).toFixed(2);
+  let finalTotal = (Number(itemTotal) + Number(tax)).toFixed(2);
+  $("#totprice").text(itemTotal);
+  $("#tax").text(tax);
+  $(".total_price").text(finalTotal);
+  $("#totalprice").val(finalTotal);
+};
 $(function() {
   $('.menu-item').on('click', function(event) {
 
+    // let quant= $(".quantity").val();
+    // console.log(quant);
     let $title = $(this).find(".menu-title").text();
     let $price = $(this).find(".price").text();
     let sterPrice = Number($price.slice(1));
-    total = total + sterPrice;
+    sterPrice = sterPrice*value;
+    getTotal(sterPrice);
+    // total = total + (sterPrice * Number(value));
     let $id = $(this).find(".item-id").text();
-    let itemTotal = total.toFixed(2);
-    let tax = (itemTotal * 0.13).toFixed(2);
-    let finalTotal = (Number(itemTotal) + Number(tax)).toFixed(2);
+    // let itemTotal = total.toFixed(2);
+    // let tax = (itemTotal * 0.13).toFixed(2);
+    // let finalTotal = (Number(itemTotal) + Number(tax)).toFixed(2);
     $(".form-items").append(addOrderItem($title, $price, $id));
-    $("#totprice").text(itemTotal);
-    $("#tax").text(tax);
-    $(".total_price").text(finalTotal);
-    $("#totalprice").val(finalTotal);
+    // $("#totprice").text(itemTotal);
+    // $("#tax").text(tax);
+    // $(".total_price").text(finalTotal);
+    // $("#totalprice").val(finalTotal);
   })
+  // $(".form-items").on("change", "input", function(event) {
+  //   value = $(this).val(); //incremented input value
+  //   // alert(value);
+  // });
+
+  $(".form-items").on("change", "input", function(event) {
+    const increment= $(this).val();
+    const itemId = $(this).attr("data-input-id");
+    console.log(itemId);
+    const itemPrice = $(".menu-items").find(`[data-item-id="${itemId}"]`).text();
+    console.log(itemPrice);
+    let sterPrice = Number(itemPrice.slice(1));
+    let price = sterPrice * increment;
+    price = price.toFixed(2);
+    $(this).siblings(".item-price").text(`$ ${price}`);
+    getTotal(price);
+  })
+
 });
 
+
+
+const getQuntity = () => {
+   console.log($(".quantity").value);
+
+}
