40dfd338405bc0dc5fd5b7b87247b4a16405951d
HermSidhu <h-sidhu@hotmail.com>
2019-11-22 03:29:47 +0000

    order history with pics

--- server.js
+++ server.js
@@ -46,6 +46,7 @@ const logoutRoutes = require("./routes/logout");
 const ordersRoutes = require("./routes/orders");
 const signupRoutes = require("./routes/signup");
 const confirmRoutes = require("./routes/confirm");
+const orderhistoryRoutes = require("./routes/orderhistory");
 
 // const dataHelpers = require(".");
 
@@ -62,6 +63,7 @@ app.use("/orders", ordersRoutes(db));
 app.use("/signup", signupRoutes(db));
 app.use("/logout", logoutRoutes());
 app.use("/confirm", confirmRoutes(db));
+app.use("/orderhistory", orderhistoryRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
