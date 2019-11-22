9fd965d519d141c6a3b7aa8c0ce2a04f1fb700c4
johnarvi <arvind.john@mail.utoronto.ca>
2019-11-22 14:43:19 +0000

    we fixed the 404 status page redirection

--- errorPage.js
+++ errorPage.js
@@ -10,29 +10,20 @@ const errorPage = (status, redirect, msg) => {
     <!-- Fonts -->
     <!-- Bootstrap CSS-->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <!-- Custom CSS -->
-    <link rel="stylesheet" href="/styles/layout.css" type="text/css" />
-
-    <!-- Font Awesome -->
-    <script src="https://kit.fontawesome.com/096d0c019b.js" crossorigin="anonymous"></script>
-    <!-- JQuery -->
-    <script type="text/javascript" src="/vendor/jquery-3.0.0.js"></script>
-    <!-- Custom JavaScript -->
-    <script type="text/javascript" src="/scripts/app.js"></script>
-    <!-- Bootstrap JS -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </head>
 
 <body class="bg-dark">
 
-<main class="d-flex justify-content-between login-main">
+<main class="m-auto text-center w-50">
 
-  <section class="log-in mr-4">
+  <section class="log-in mr-4 mt-5">
     <h1 class="text-light">${msg}</h1>
-    <a href="${redirect}"><h1 class="text-light">Click here to get back to the restaurants</h1></a>
-  </section>
+    </section>
+  <section class="log-in mr-4">
+    <h1 class="text-light">Go back to <a href="${redirect}"><span class="text-info">FoodByte</a> </span></h1>
+   <div> <img src="https://i.pinimg.com/originals/77/8e/29/778e294a406890bc49c93886dc2e5f2e.gif"> </div>
+    </section>
 </main>
 
 
